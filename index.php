f7d76863ba118cfb869cf77217286f0a71924b1c
KurilHohenzollern <pojarius@yandex.ru>
2021-06-18 22:23:52 +0400

    add helloworld

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 
+header('Cache-control: private, max-age=0');
 // Печатает год в STDOUT
 echo date('Y');
 echo "\n";
 echo "Hello, World!";
+
+foreach ($_SERVER as $parm => $value)  
+echo "$parm = '$value'\n";
+
+foreach ($_GET as $parm => $value)
+echo"$parm = '$value'\n";
